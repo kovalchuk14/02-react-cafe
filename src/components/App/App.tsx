3da646a39ac1c0d49c8636da2cac5d290e7da50e
kovalchuk14 <kovalcukanton682@gmail.com>
2025-07-16 23:14:56 +0200

    path fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import css from './App.module.css'
 import CafeInfo from '../CafeInfo/CafeInfo'
 import { useState } from 'react'
 import type { Votes,VoteType } from '../../types/votes';
-import VoteOptions from '../VoteOprions/VoteOptions';
+import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
 
