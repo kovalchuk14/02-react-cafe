0a4756725ca22b4019bbfc7194045a6e8b4523fe
kovalchuk14 <kovalcukanton682@gmail.com>
2025-07-17 13:35:04 +0200

    fix

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -6,18 +6,16 @@ interface VoteOptionsProps {
     canReset: boolean,
 }
 
-import css from "./VoteOptions.module.css"
+import css from "./VoteOptions.module.css";
 
 export default function VoteOptions({ onVote, onReset, canReset }:VoteOptionsProps) {
 
-    return (
-      <>
-        <div className={css.container}>
-          <button onClick = {()=>onVote("good")} className={css.button}>Good</button>
-          <button onClick = {()=>onVote("neutral")} className={css.button}>Neutral</button>
-          <button onClick = {()=>onVote("bad")} className={css.button}>Bad</button>
-          {canReset && (<button onClick={onReset} className={`${css.button} ${css.reset}`}>Reset</button>)}
-        </div>  
-      </>
-    )
+  return (
+    <div className={css.container}>
+      <button onClick={() => onVote("good")} className={css.button}>Good</button>
+      <button onClick={() => onVote("neutral")} className={css.button}>Neutral</button>
+      <button onClick={() => onVote("bad")} className={css.button}>Bad</button>
+      {canReset && (<button onClick={onReset} className={`${css.button} ${css.reset}`}>Reset</button>)}
+    </div>
+  );
 }
